58e174425d806b922b4f0d7b9c89fd5de3b14978
yangheng <1220626016@qq.com>
2022-01-12 21:30:03 +0800

    feat: 修改格式

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
-    <Avatar
-      :size="50"
-      url="https://img2.baidu.com/it/u=1685092271,2574681286&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=797"
-    />
-    <Icon type="home" />
-    <Pager />
-  </div>
+    <div id="app">
+        <Avatar
+            :size="50"
+            url="https://img2.baidu.com/it/u=1685092271,2574681286&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=797"
+        />
+        <Icon type="home" />
+        <Pager />
+    </div>
 </template>
 
 <script>
@@ -14,19 +14,19 @@ import Avatar from "./components/Avatar.vue";
 import Icon from "./components/Icon.vue";
 import Pager from "./components/Pager.vue";
 export default {
-  name: "App",
-  components: {
-    Avatar,
-    Icon,
-    Pager,
-  },
+    name: "App",
+    components: {
+        Avatar,
+        Icon,
+        Pager,
+    },
 };
 Promise.all();
 </script>
 
 <style scped>
 .icon-container {
-  font-size: 26px;
-  color: red;
+    font-size: 26px;
+    color: red;
 }
 </style>
